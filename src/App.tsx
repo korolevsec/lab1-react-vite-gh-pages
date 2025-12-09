381a89fef566f2b7fa37b4e85c140096dca22ee0
korolevsec <korolevma@tksu.ru>
2025-12-09 23:41:22 +0300

    feat: init Vite + React + TypeScript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Counter from './components/Counter.tsx'
+import UserTable from './components/UserTable'
 import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <Counter /> {/* ← Теперь здесь только компонент */}
+      <UserTable />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
