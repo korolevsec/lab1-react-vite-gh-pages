6f789151635c1aa40b09d6dd57ee9b39e857fe3b
korolevsec <korolevma@tksu.ru>
2025-12-09 22:41:43 +0300

    feat: init Vite + React + TypeScript project

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,9 @@
     import '@testing-library/jest-dom'; // Импортируем расширения для expect
+    import '@testing-library/jest-dom'
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+
+// Очищаем DOM после каждого теста
+afterEach(() => {
+  cleanup()
+})
